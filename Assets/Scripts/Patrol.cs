fa520f53d03587723f988530a00a5414607db422
Stijn Stroeckx <stijn.stroeckx@student.howest.be>
2017-07-01 11:23:09 +0200

    Added smooth cornering

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -7,15 +7,20 @@ public class Patrol : MonoBehaviour
     public Transform[] Waypoints;
     public float Speed = 5f;
     public float CutCornerDistance = 5f;
-    public int _currentWayPoint = 0;
-    public int _nextWayPoint = 0;
+    private bool _isDoingSmoothCorner = false;
+    private int _currentWayPoint = 0;
+    private int _previousWaypoint = 0;
     private Quaternion _lookRotation;
+    private Vector3 _target;
     private Vector3 _direction;
     private bool _touched = false;
     public ParticleSystem ParticleSys;
-
+    private float _cutCornerFactor = 0;
+    public float MaxCutCornerFactor = 5;
+    public float CornerIncrementSpeed = 0.1f;
     void Start()
     {
+        _previousWaypoint = Waypoints.Length - 1;
         ParticleSys.Stop();
         // _wayPoints.Add(Vector3.zero);
         //for(int i = 0; i < Waypoints.Length)
@@ -40,17 +45,51 @@ public class Patrol : MonoBehaviour
 
     void Update()
     {
-        //float factor = 250;
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            int next = i + 1;
+            if (next >= Waypoints.Length)
+            {
+                next = 0;
+            }
+            Debug.DrawLine(Waypoints[i].position, Waypoints[next].position,Color.black);
+            
+        }
         if (!_touched)
         {
-            /*if ((Waypoints[_currentWayPoint].position - transform.position).magnitude < CutCornerDistance)
+            //if we are within cutcorner distance, start cutting corner
+            if ((Waypoints[_currentWayPoint].position - transform.position).magnitude < CutCornerDistance)
             {
-                Vector3 smoothDirection = (Waypoints[_currentWayPoint].position - Waypoints[_currentWayPoint].position);
-                
+                _isDoingSmoothCorner = true;
+                _cutCornerFactor = 0;
+                if (Waypoints.Length > 0)
+                    AddCurrentWaypoint();
+            }
+            //if we are on the line to the next point, stop cutting corner
+            if ((Waypoints[_currentWayPoint].position - transform.position).magnitude + (Waypoints[_previousWaypoint].position - transform.position).magnitude == (Waypoints[_currentWayPoint].position - Waypoints[_previousWaypoint].position).magnitude)
+                _isDoingSmoothCorner = false;
+            if (_isDoingSmoothCorner)
+            {
+                if (_cutCornerFactor < MaxCutCornerFactor)
+                {
+                    _cutCornerFactor += CornerIncrementSpeed;
+                }
+                Vector3 smoothDirection = (Waypoints[_currentWayPoint].position - Waypoints[_previousWaypoint].position);
                 smoothDirection.Normalize();
-                _direction = ((smoothDirection * factor) - transform.position).normalized;
-            }*/
-            _direction = (Waypoints[_currentWayPoint].position - transform.position).normalized;
+                _target = (Waypoints[_previousWaypoint].position + (smoothDirection * _cutCornerFactor));
+                if ((_target - transform.position).magnitude < 0.1f)
+                {
+                    _target = Waypoints[_currentWayPoint].position;
+                }
+                _direction = (_target - transform.position).normalized;
+                Debug.DrawLine(transform.position, _target, Color.red);
+            }
+            else
+            {
+                _target = Waypoints[_currentWayPoint].position;
+                _direction = (_target - transform.position).normalized;
+            }
+
             // Only turn if we aren't on top of the target point
             if (_direction.magnitude > 0.0f)
             {
@@ -66,7 +105,8 @@ public class Patrol : MonoBehaviour
             }
             //Vector3 lerpValue = Vector3.Lerp(TransSelf.position + TransSelf.forward, Waypoints[_currentWayPoint].position, Time.deltaTime * 2);
             //TransSelf.LookAt(lerpValue/*Waypoints[_currentWayPoint].position*/);
-            TransSelf.position = Vector3.MoveTowards(TransSelf.position, Waypoints[_currentWayPoint].position, Speed * Time.deltaTime);
+            Debug.DrawLine(transform.position, _target, Color.green);
+            TransSelf.position = Vector3.MoveTowards(TransSelf.position, _target, Speed * Time.deltaTime);
         }
     }
 
@@ -77,10 +117,10 @@ public class Patrol : MonoBehaviour
         {
             _currentWayPoint = 0;
         }
-        ++_nextWayPoint;
-        if (_nextWayPoint >= Waypoints.Length)
+        _previousWaypoint = _currentWayPoint - 1;
+        if (_currentWayPoint == 0)
         {
-            _nextWayPoint = 0;
+            _previousWaypoint = Waypoints.Length - 1;
         }
     }
 

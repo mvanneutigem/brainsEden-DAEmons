a533a8b1d1f1a672a262d6a26d6a0dd127492d83
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-07-01 22:46:17 +0200

    wigglewiggle

---
+++ Assets/Scripts/Wiggle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggle : MonoBehaviour
+{
+
+    private float _elapsed = 0.0f;
+    public void WiggleMethod()
+    {
+        Debug.Log("Wiggle");
+        _elapsed += Time.deltaTime*10;
+        transform.Rotate(Vector3.Cross(transform.localRotation.eulerAngles, Vector3.right), (float) Mathf.Sin(_elapsed)*10);
+    }
+}

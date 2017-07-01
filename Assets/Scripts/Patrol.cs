f791cb21bd5cb672650745e8798a25f339c3608a
Stijn Stroeckx <stijn.stroeckx@student.howest.be>
2017-07-01 20:46:10 +0200

    field of view fix

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -223,7 +223,9 @@ public class Patrol : MonoBehaviour
         Debug.DrawLine(transform.forward, transform.forward * 5, Color.blue);
         if ((Player.transform.position - transform.position).magnitude < _DetectionRadius)
         {
-            if (Vector3.Angle(Player.transform.position, transform.forward) % ((360 * Math.PI)/180) < 30)
+            float test = Vector3.Angle((Player.transform.position - transform.position), transform.forward);
+            test = Mathf.Repeat(test, 360);
+            if (test < 60.0f)
             {
                 RaycastHit hit;
                 Vector3 direction = (Player.transform.position - transform.position).normalized;
